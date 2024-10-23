869d00810c621aa991ac159dcd2e940b13d68d1c
jimmyndegizi <ndegwajames123@gmail.com>
2024-10-23 20:48:39 +0300

    Task form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // get form and task list elements
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  //handle submissions
+  form.addEventListener("submit",function(event){
+  event.preventDefault();
+
+  //get task description
+const taskDescription = document.getElementById("new-task-description").value;
+
+// Check if the task description is not empty
+if (taskDescription !== "") {
+  // Create a new list item for the task
+  const li = document.createElement("li");
+  li.textContent = taskDescription;
+
+  // Append the task to the task list
+  taskList.appendChild(li);
+
+  // Clear the input field after submission
+  document.getElementById("new-task-description").value = "";
+  }
+});
 });
